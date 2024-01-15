67d88cdf71238a281260865b0f4a1adffb801641
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2024-01-15 08:59:46 +0700

    🎨 ⚡️ ✨ create login ui

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Input from "../../component/input";
+import React from "react";
+import Button from "../../component/Button";
+
+export default function Home() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="p-5">
+      <h1 className="pb-5 font-bold text-2xl">Login</h1>
+      <Input
+        label="Email"
+        type="email"
+        name="email"
+        value={email}
+        onChange={handleEmailChange}
+        placeholder="Enter your email"
+      />
+      <Input
+        label="password"
+        type="password"
+        name="Password"
+        value={password}
+        onChange={handlePasswordChange}
+        placeholder="Enter your password"
+      />
+      <Button
+        label="Login"
+        onClick={handleLogin}
+        disabled={!email || !password}
+      />
+    </div>
+  );
+}
