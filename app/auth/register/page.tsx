431181db9547225da92fca403d742f0de3616adf
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2024-01-15 09:05:34 +0700

    ⚡️ 🎨 create register user ui

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -3,9 +3,14 @@ import Input from "../../component/input";
 import React from "react";
 import Button from "../../component/Button";
 
-export default function LoginPage() {
+export default function RegisterPage() {
+  const [username, setusername] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setusername(event.target.value);
+  };
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -16,13 +21,22 @@ export default function LoginPage() {
   };
 
   const handleLogin = () => {
+    console.log("Username:", username);
     console.log("Email:", email);
     console.log("Password:", password);
   };
 
   return (
     <div className="p-5">
-      <h1 className="pb-5 font-bold text-2xl">Login</h1>
+      <h1 className="pb-5 font-bold text-2xl">Register</h1>
+      <Input
+        label="Username"
+        type="text"
+        name="username"
+        value={username}
+        onChange={handleUsernameChange}
+        placeholder="Enter your username"
+      />
       <Input
         label="Email"
         type="email"
@@ -33,18 +47,18 @@ export default function LoginPage() {
       />
       <Input
         label="password"
-        type="password"
+        type="text"
         name="Password"
         value={password}
         onChange={handlePasswordChange}
         placeholder="Enter your password"
       />
       <Button
-        label="Login"
+        label="Register"
         onClick={handleLogin}
         disabled={!email || !password}
       />
-      <p>sign up </p>
+      <p>Login</p>
     </div>
   );
 }
